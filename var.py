a42d4fa288b2be4555eb84bf540cead73216a132
Vlad <vsakayafignavlad@gmail.com>
2019-12-06 23:18:53 +0300

    Update var.py

--- var.py
+++ var.py
@@ -1,2 +1,2 @@
-token_of_bot = '1025530719:AAHsC8-WtGNPD2SRSMBi2UQ_L6lqSroi-7Y'
+token_of_bot = '833025627:AAFGvHjM4eB4ypPZr9Q3Fqsf0Ll7MjiY8N0'
 token_of_disk = 'AgAAAAAcO-LKAAXwFv7smI1_XUIkurUJQFW8QiM'
