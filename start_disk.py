8817722c638d0f4312ef1edfbdbc4c44b186f196
Vlad <vsakayafignavlad@gmail.com>
2019-12-06 15:06:50 +0300

    1

--- start_disk.py
+++ start_disk.py
@@ -1,15 +1,20 @@
-def main(id,FirstName,SecondName):
+def main(id, FirstName, SecondName):
     import json
 
     import var
     import os
 
-
-    person_dict = {'Id' : str(id),
-                   'FirstName' : str(FirstName),
-                   'SecondName' : str(SecondName),
-                   'Balance' : '0',
-                   'Status' : 'Student'}
+    person_dict = {"Id": str(id),
+                   "WebsiteUsername": "",
+                   "WebsitePassword": "",
+                   "WebsiteSsid": "",
+                   "TelegramSurname": str(SecondName),
+                   "TelegramName": str(FirstName),
+                   "InviteCode": "",
+                   "Surname": "",
+                   "Name": "",
+                   "Balance": "0",
+                   "Group": "Student"}
 
     print(person_dict)
     print('###')
@@ -17,25 +22,10 @@ def main(id,FirstName,SecondName):
 
     newUser = False
 
-    with open('people.json', 'r') as f:
+    with open('/home/project/database/people.json', 'r') as f:
         data = json.loads(f.read())
         for i in data:
-            for j in i:
-                if j == 'Id':
-                    us.append(i[j])
-        if id in us:
-            newUser = False
-        else:
-            newUser = True
 
 
-    if newUser:
-        print('data 1',data)
-        data.append(person_dict)
-        print('data 2', data)
-        print('###')
-
-    with open('people.json','w') as file:
-        json.dump(data, file, indent=2,ensure_ascii=False)
-
-
+    with open('people.json', 'w') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
