f176706498fcc6c5864d9b2e97f22378ae0175fe
Vlad <vsakayafignavlad@gmail.com>
2019-12-06 16:45:53 +0300

    Update prsn_inf.py

--- prsn_inf.py
+++ prsn_inf.py
@@ -5,18 +5,9 @@ import os
 
 def info(id):
     us = []
-    with open('people.json', 'r') as f:
+    with open('/home/project/database/users.json', 'r') as f:
         data = json.loads(f.read())
         for i in data:
             if i["Id"] == str(id):
                 print(i)
                 return i
-
-
-def dell():
-    os.remove('people.json')
-    # for k in j:
-    #     if k == Username:
-    #         while 1:
-    #             print('username')
-    #         return j
