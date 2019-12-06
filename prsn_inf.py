ecf14cfe5135049acedf43d80bf46e538c867771
Vlad <vsakayafignavlad@gmail.com>
2019-12-06 19:18:41 +0300

    Update prsn_inf.py

--- prsn_inf.py
+++ prsn_inf.py
@@ -9,7 +9,7 @@ def info(id):
     with open('/home/project/database/users.json', 'r') as f:
         data = json.loads(f.read())
         for i in data:
-            print(i["Id"], '#', +id)
+            print(i)
             if i["Id"] == str(id):
                 print('info')
                 print(i)
