199066870260240fd1313fb9c636223602c6576f
ivaleo <58272828+ivaleo@users.noreply.github.com>
2019-12-08 19:33:36 +0300

    Update start_disk.py

--- start_disk.py
+++ start_disk.py
@@ -1,3 +1,5 @@
+from print_data import *
+
 def main(Id, message):
     import json
     import var
@@ -6,21 +8,15 @@ def main(Id, message):
     print('after /start')
     print('msg:', message, 'id:', Id)
     
-    # data = load_users()
-    with open('/home/project/database/users.json', 'r') as file:
-        data = json.loads(file.read())
-        file.close()
+    data = load_users()
         
     # добавление ChatId нового пользователя по пригласительному коду
-    	for user in data:
-            if user['InviteCode'] == message:
-                if user['TelegramChatId'] == '':
-                    indexx = data.index(user)
-                    data[indexx]['TelegramChatId'] = str(Id)
-                    print('The new ChatId has been set done.')
-                break
+    for user in data:
+        if user['InviteCode'] == message:
+            if user['TelegramChatId'] == '':
+                indexx = data.index(user)
+                data[indexx]['TelegramChatId'] = str(Id)
+                print('The new ChatId has been set done.')
+            break
 
-    # save_users(data)
-    with open('/home/project/database/users.json', 'w') as file:
-        json.dump(data, file, indent=2, ensure_ascii=False)
-        file.close()
+    save_users(data)
