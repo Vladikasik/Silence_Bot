eb7c82a6ff06ee93ae88d8cbdc7daedb991fab3d
Vlad <vsakayafignavlad@gmail.com>
2019-12-11 15:05:51 +0300

    Update test11.py

--- test11.py
+++ test11.py
@@ -7,11 +7,11 @@ def tesstt11(msg):
         main(msg,i)
 def main(msg,i):
     if i < 10:
-        lol = msg.text
         bot = telebot.TeleBot(var.token_of_bot)
         i += 1
-        bot.register_next_step_handler(bot.send_message(msg.chat.id, lol),
-                                       main(msg,i))
-        time.sleep(2)
-    else:
-        print('done')
+        bot.register_next_step_handler(bot.send_message(msg.chat.id, 'ok'),
+                                       main1(msg,i))
+
+def main1(msg,i):
+    print(msg.text)
+    main(msg,i)
