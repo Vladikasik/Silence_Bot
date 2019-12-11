9e62d1c5ee561d8269d99984796db2436fd2b6bf
Vlad <vsakayafignavlad@gmail.com>
2019-12-11 13:15:24 +0300

    Update test11.py

--- test11.py
+++ test11.py
@@ -5,7 +5,11 @@ def tesstt11(msg):
     if msg.text == '909':
         main(msg,i)
 def main(msg,i):
-    bot = telebot.TeleBot(var.token_of_bot)
-    i+=1
-    bot.register_next_step_handler(bot.send_message(msg.chat.id,str(i)),
-                                                        main(msg,i))
+    if i < 10:
+        lol = msg.text
+        bot = telebot.TeleBot(var.token_of_bot)
+        i += 1
+        bot.register_next_step_handler(bot.send_message(msg.chat.id, lol),
+                                       main(msg, i))
+    else:
+        print('done')
