2122736396b43317b156bf9ec4653e13460f5aef
ivaleo <58272828+ivaleo@users.noreply.github.com>
2019-12-11 20:45:28 +0300

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -1,8 +1,19 @@
 from telebot import types
 
+# кнопки для обычного пользователя
 bttn_send = '⬆️Перевод'
 bttn_info = '🥭Баланс'
 
 markup = types.ReplyKeyboardMarkup()
 markup.row(bttn_send)
 markup.row(bttn_info)
+
+# кнопки для админа
+bttn_yes = 'Да'
+bttn_no = 'Нет'
+bttn_later = 'Отложить'
+
+markup_admin = types.ReplyKeyboardMarkup()
+markup_admin.row(bttn_yes)
+markup_admin.row(bttn_no)
+markup_admin.row(bttn_later)
