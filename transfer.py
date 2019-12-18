dd1d5b02e7a36ff239086a0fd2d05dc38a4dd0cb
ivaleo <58272828+ivaleo@users.noreply.github.com>
2019-12-18 22:24:57 +0300

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -38,27 +38,21 @@ class trans:
 	
     def availavle_for_student(self):
 
-		if int(self.transfer_sum) <= self.balance_1 and int(self.transfer_sum) > 0 and self.user_exists :
-			self.available_to_trans = True
-		else:
-			self.available_to_trans = False
+        if int(self.transfer_sum) <= self.balance_1 and int(self.transfer_sum) > 0 and self.user_exists :
+            self.available_to_trans = True
+        else:
+            self.available_to_trans = False
 
-		self.transfer_sum_massiv = list(self.transfer_sum)
+        self.transfer_sum_massiv = list(self.transfer_sum)
 
-		intt = '0123456789'
+        intt = '0123456789'
 
-		for item in self.transfer_sum_massiv:
-			if item in intt:
-				self.available_to_trans_1 = True
-			else:
-				self.available_to_trans_1 = False
-				break
-
-		if len(self.massiv) == 2:
-			pass
-		else:
-			self.available_to_trans = False
-			self.available_to_trans_1 = False
+        for item in self.transfer_sum_massiv:
+            if item in intt:
+                self.available_to_trans_1 = True
+            else:
+                self.available_to_trans_1 = False
+                break
 
 		return self.available_to_trans and self.available_to_trans_1
 
@@ -76,57 +70,51 @@ class trans:
 			else:
 				self.available_to_trans_1 = False
 				break
-
-		if len(self.massiv) == 2:
-			pass
-		else:
-			self.available_to_trans = False
-			self.available_to_trans_1 = False
+            
 		return self.available_to_trans and self.available_to_trans_1
 
     def printt_before(self):
-		print('###')
-		print('Transf_sum',self.transfer_sum)
-		print('Userreturn with their balances before transer')
-		print(self.User1['Surname'], self.Username_2)
-		print(self.balance_1,self.balance_2)
-		print('###')
+        print('###')
+        print('Transf_sum',self.transfer_sum)
+        print('Userreturn with their balances before transer')
+        print(self.User1['Surname'], self.Username_2)
+        print(self.balance_1,self.balance_2)
+        print('###')
 
     def printt_after(self):
-		print('###')
-		print('Transf_sum',self.transfer_sum)
-		print('Users with their balances after transfer')
-		print(self.User1['Surname'], self.Username_2)
-		self.balance_1 = get_user(self.id)['Balance']
-		self.balance_2 = get_user_by_surname(self.Username_2)['Balance']
-		print(self.balance_1,self.balance_2)
-		print('###')
+        print('###')
+        print('Transf_sum',self.transfer_sum)
+        print('Users with their balances after transfer')
+        print(self.User1['Surname'], self.Username_2)
+        self.balance_1 = get_user(self.id)['Balance']
+        self.balance_2 = get_user_by_surname(self.Username_2)['Balance']
+        print(self.balance_1,self.balance_2)
+        print('###')
 
     def do_student(self):
-		#детям нельзя переводить
+        #детям нельзя переводить
         return
 
     def do_teacher(self):
-		# добавление транзакции
-		time1 = time.strftime('%H %M %S %d %m').split()
-		time2 = ''
-		time3 = [':', ':', '_', '', '']
-		for i in range(0, len(time1)):
-			time2 += time1[i]
-			time2 += time3[i]
+        # добавление транзакции
+        time1 = time.strftime('%H %M %S %d %m').split()
+        time2 = ''
+        time3 = [':', ':', '_', '', '']
+        for i in range(0, len(time1)):
+            time2 += time1[i]
+            time2 += time3[i]
 
-		trans_inf = {
-			'From:': self.User1['Surname'],
-			'To:': self.Username_2,
-			'Value:': self.transfer_sum,
-			'Time:': time2
-		}
+        trans_inf = {
+            'From:': self.User1['Surname'],
+            'To:': self.Username_2,
+            'Value:': self.transfer_sum,
+            'Time:': time2
+        }
 		
-		data = load_operations()
-		data.append(trans_inf)
-		save_operations(data)
+        data = load_operations()
+        data.append(trans_inf)
+        save_operations(data)
 		
-		data = load_pending()
-		data.append(trans_inf)
-		save_pending(data)
-
+        data = load_pending()
+        data.append(trans_inf)
+        save_pending(data)
